bc09d7b5cecd741dba94abd637a66f42d19779b1
Sinceter <88071472+Sinceter@users.noreply.github.com>
2025-08-19 16:46:43 +0800

    Delete KT_mNGS_analysis_Rhoda/work.sh

--- KT_mNGS_analysis_Rhoda/work.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-bash ./mNGS_analysis.sh --start_bc 01 \
-	--end_bc 24 \
-	--raw_dir <folder/name/under/no_sample_id> \
-	--data_hps  12 \
-	--kraken2_db ppf_only \
-	--csv sample_template.csv
